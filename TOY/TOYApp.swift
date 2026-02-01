0d6155e5eb09681bf5528de1fc74fa4e8afde876
davidvypark <davidvypark@gmail.com>
2026-02-01 17:28:10 +0700

    Initial Commit

---
+++ TOY/TOYApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOYApp.swift
+//  TOY
+//
+//  Created by David Park on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TOYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
